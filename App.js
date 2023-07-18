2c237817b6ea9adf94ac7281c93eeaa0ac305b8a
VujevicStipe <75379483+VujevicStipe@users.noreply.github.com>
2023-07-18 17:31:08 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,17 +3,6 @@ import "./App.css";
 import Board from "./components/Board";
 
 function App() {
-  //   function makeSquares(number) {
-  //     const array = [];
-  //     for (let i = 0; i < number; i++) {
-  //       array.push(i);
-  //     }
-
-  //     return array;
-  //   }
-
-  //   console.log(makeSquares(9));
-
   return (
     <>
       <Board />
